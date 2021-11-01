2517139b1bed09e81550638f260c900b2dfc1718
Paul Downey <psd@whatfettle.com>
2021-11-01 20:05:58 +0000

    Lambeth's trees and preservation orders

--- collection/pipeline.mk
+++ collection/pipeline.mk
@@ -1,10 +1,21 @@
+TREE_DATASET=$(DATASET_DIR)tree.csv
+TREE_TRANSFORMED_FILES=\
+    $(TRANSFORMED_DIR)tree/8f35e0a7bbca0daeda982d6f39aac0afa7b0fae1bf9419c53a5d279f450ad7b3.csv
+
+$(TRANSFORMED_DIR)tree/8f35e0a7bbca0daeda982d6f39aac0afa7b0fae1bf9419c53a5d279f450ad7b3.csv: collection/resource/8f35e0a7bbca0daeda982d6f39aac0afa7b0fae1bf9419c53a5d279f450ad7b3
+	$(run-pipeline)
+
+$(TREE_DATASET): $(TREE_TRANSFORMED_FILES)
+	$(build-dataset)
+
+transformed:: $(TREE_TRANSFORMED_FILES)
+
+dataset:: $(TREE_DATASET)
+
+
 TREE_PRESERVATION_ORDER_DATASET=$(DATASET_DIR)tree-preservation-order.csv
 TREE_PRESERVATION_ORDER_TRANSFORMED_FILES=\
-    $(TRANSFORMED_DIR)tree-preservation-order/61ed041ab7f53b34b84519af6b2e80563046dc7f62bb079ae530a0dde8e52e90.csv\
     $(TRANSFORMED_DIR)tree-preservation-order/8f35e0a7bbca0daeda982d6f39aac0afa7b0fae1bf9419c53a5d279f450ad7b3.csv
-
-$(TRANSFORMED_DIR)tree-preservation-order/61ed041ab7f53b34b84519af6b2e80563046dc7f62bb079ae530a0dde8e52e90.csv: collection/resource/61ed041ab7f53b34b84519af6b2e80563046dc7f62bb079ae530a0dde8e52e90
-	$(run-pipeline)
 
 $(TRANSFORMED_DIR)tree-preservation-order/8f35e0a7bbca0daeda982d6f39aac0afa7b0fae1bf9419c53a5d279f450ad7b3.csv: collection/resource/8f35e0a7bbca0daeda982d6f39aac0afa7b0fae1bf9419c53a5d279f450ad7b3
 	$(run-pipeline)
